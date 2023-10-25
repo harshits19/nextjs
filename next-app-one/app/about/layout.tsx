34f4b1a2b49efaacd5a94cd9003e6fb7567ebde2
Harshit <gaurharshit4@gmail.com>
2023-10-26 02:37:57 +0530

    added users

--- next-app-one/app/about/layout.tsx
+++
@@ -1,9 +0,0 @@
-const AboutLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-5">
-      {"hello"}
-      {children}
-    </div>
-  )
-}
-export default AboutLayout
--- next-app-one/app/page.tsx
+++ next-app-one/app/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!
-      <Link href="/about">About Page</Link>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <p className="text-5xl font-bold">Next APP</p>
+      <Link href="/users" className="underline">Users Page</Link>
     </main>
   )
 }
